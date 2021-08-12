ac45a8d2402de0fc9735d0df92eb7e6b81d69af5
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2021-08-12 22:29:14 +0100

    Done and dusted

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes/api');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//connect to the database
+mongoose.connect(process.env.DB, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+mongoose.connection.on("open", function () {
+  console.log("Mongoose Connected Successfully!");
+});
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+app.use('/api', routes);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  next();
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port http://localhost:/${port}`)
+});
