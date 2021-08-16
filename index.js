8e374d8e629de7b5c0c137914553a0ae3459d533
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2021-08-17 00:46:11 +0100

    Fixed minor bug

--- index.js
+++ index.js
@@ -34,5 +34,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port http://localhost:/${port}`)
+  console.log(`Server running on port http://localhost:${port}`)
 });
